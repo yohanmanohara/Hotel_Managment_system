e8df77b479e5c27c4837a7f9821774102210316e
yohanmanohara <warushayohan80@gmail.com>
2025-05-04 10:48:30 +0530

    made finalize it

--- src/main/java/com/example/hotel_management/controllers/HomeController.java
+++ src/main/java/com/example/hotel_management/controllers/HomeController.java
@@ -7,6 +7,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public String home() {
-        return "Hotel Management API is running! Use /api/hotels for hotel data";
+        return "<html>" +
+                "<head><title>Hotel Management API</title></head>" +
+                "<body style='display:flex; justify-content:center; align-items:center; height:100vh;'>" +
+                "<div style='text-align:center; font-family:sans-serif;'>" +
+                "<h1>Hotel Management API is runn wenawa uttaneeee!</h1>" +
+                "<p>Use <code>/api/hotels</code> for hotel data</p>" +
+                "</div>" +
+                "</body>" +
+                "</html>";
     }
 }
